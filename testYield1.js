d22b6ed197a035f818378f8023a2faf3771b68df
张炯 <zj@yuan-mai.com>
2016-08-29 17:15:21 +0800

    修改传参

--- testYield1.js
+++ testYield1.js
@@ -3,7 +3,7 @@ console.log("step in child generator")
 var b = yield 'running';
 console.log(b);
 console.log("step out child generator")
-return 'ss';
+//return 'ss';
 }
 
 
@@ -12,7 +12,7 @@ function* start(){
 	var a = yield 'start';
 	console.log(a);//输入为undefined
 	var b = yield *runGenerator;
-	console.log(b);
+	console.log(b);//b不接受传入的gg参数
 	var c = yield 'end';
 	console.log(c);
 	return 'over';
@@ -23,6 +23,6 @@ console.log(start.constructor.name);
 var it = start();
 // console.log(it.current());
 console.log(it.next());//等待 start返回  
-console.log(it.next());//等待runGenerator返回
+console.log(it.next('gg'));//等待runGenerator返回
 console.log(it.next(333));//等待running返回
 console.log(it.next(44));
